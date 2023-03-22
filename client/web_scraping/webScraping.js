397ac5b2120b09e349c8bef19ec5a31322428336
michael seaman <mseaman26@gmail.com>
2023-03-22 11:38:16 -0700

    supper lotto data formatted

--- client/web_scraping/webScraping.js
+++ client/web_scraping/webScraping.js
@@ -8,11 +8,15 @@ axios.get(superLottoPlusResults)
   .then(response => {
     const $ = cheerio.load(response.data);
     $('.c-result-card').each((i, el) => {
-        let nums = ''
+        let nums = []
         $(el).find('ul').children().each((i, num) => {
-            nums += $(num).text().concat(', ').trim()
+            nums.push($(num).text().trim())
         })
+        for(let i = 0; i < nums.length; i++){
+            nums[i] = nums[i].split('\n')[0]
+        }
         console.log($(el).find('time').text(), nums)
+        console.log(nums.length)
     })
 
   })
